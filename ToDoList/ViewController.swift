69c1df16c28167b990be3882ad0bbe7acb8c8e80
Daniyar <mamadovd@gmail.com>
2021-06-07 15:09:52 +0300

    Initial commit. Create project & repository

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Daniyar Mamadov on 04.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
